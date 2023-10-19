eb8844c3558f1e4571cd6c21595acd9a1989efca
manoj-65 <manoj.y@testyantra.in>
2023-10-19 14:53:26 +0530

    Logger and testCase

--- src/test/java/org/alpha/userapp/UserAppMongoApplicationTests.java
+++ src/test/java/org/alpha/userapp/UserAppMongoApplicationTests.java
@@ -1,13 +1,20 @@
 package org.alpha.userapp;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class UserAppMongoApplicationTests {
+	public static Logger logger = LoggerFactory.getLogger(UserAppMongoApplicationTests.class);
 
 	@Test
 	void contextLoads() {
+		logger.info("Test Case Executing");
+		assertEquals(true, true);
 	}
 
 }

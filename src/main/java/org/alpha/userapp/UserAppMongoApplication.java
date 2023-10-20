971b4a579f6aadf85d887071592eaed9cbc8d439
manoj-65 <manoj.y@testyantra.in>
2023-10-20 16:51:39 +0530

    Removing the logger

--- src/main/java/org/alpha/userapp/UserAppMongoApplication.java
+++ src/main/java/org/alpha/userapp/UserAppMongoApplication.java
@@ -1,23 +1,12 @@
 package org.alpha.userapp;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import jakarta.annotation.PostConstruct;
-
 @SpringBootApplication
 public class UserAppMongoApplication {
-	public static Logger logger = LoggerFactory.getLogger(UserAppMongoApplication.class);
-
-	@PostConstruct
-	public void init() {
-		logger.info("Application Started");
-	}
 
 	public static void main(String[] args) {
-		logger.info("Application Executed");
 		SpringApplication.run(UserAppMongoApplication.class, args);
 	}
 
